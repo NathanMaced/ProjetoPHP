f493eb5d8a46a5c64654fe12901bed20171eab3b
NathanMaced <nathanmacedo02@gmail.com>
2022-08-08 20:23:44 -0300

    segundo teste em php

--- aula01.php
+++ aula01.php
@@ -1,5 +1,24 @@
-<?php 
+	<?php
 
-	echo "Hello World!";
+		
 
- ?>
+	?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+
+	<?php
+
+		echo "Hello World!";
+
+	?>
+
+</body>
+</html>
+

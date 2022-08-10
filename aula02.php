506d1849ceab4657a8780fa35850c5e7ef97d356
NathanMaced <nathanmacedo02@gmail.com>
2022-08-10 20:27:16 -0300

    aula 02

---
+++ aula02.php
@@ -0,0 +1,23 @@
+<?php
+
+	$valor = 12;
+	$valor = 'Etecia';
+	$valor = true;
+	$valor = 25.5;
+
+	$valor = "Olá Mundo!!!";
+
+	echo "Eu escrevi algo para ver <br>";
+	print("Estou usando o print <br>");
+	print date('Y-M-D')
+
+?>
+
+<br><br>
+
+<?php
+
+	$a = 10;
+	echo $a;
+
+?>
